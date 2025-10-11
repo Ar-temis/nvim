7292f61d0b027ded930d032403f5fbcf162294a9
Ar-temis <temuulen.enkhtamir@dukekunshan.edu.cn>
2025-10-11 16:53:05 -0400

    postgres lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,16 +1,22 @@
 require("nvchad.configs.lspconfig").defaults()
 
 local servers = {
-  "html", "cssls", "ruff", "clangd",
+  "html",
+  "cssls",
+  "ruff",
+  "clangd",
   "jedi_language_server",
-  "jdtls", "ts_ls", "lua_ls"
+  "postgres_lsp",
+  "jdtls",
+  "ts_ls",
+  "lua_ls",
 }
 vim.lsp.enable(servers)
 vim.lsp.config.jedi_language_server = {
   config = {
     jediSettings = {
       debug = true,
-    }
-  }
+    },
+  },
 }
 -- read :h vim.lsp.config for changing options of lsp servers
